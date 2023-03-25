189c64bf5689af151753d36252e2af91cd2c45a4
y-w-22 <yuiko_w@outlook.jp>
2023-03-25 15:27:28 +0900

    1st commmit

---
+++ src/view.py
@@ -0,0 +1,16 @@
+#coding: utf-8
+
+from flask import Flask, render_template
+
+# Flask をインスタンス化
+app = Flask(__name__)
+
+# --- View側の設定 ---
+# ルートディレクトリにアクセスした場合の挙動
+@app.route('/')
+# def以下がアクセス後の操作
+def index():
+    #return "これ出てる？"
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run()
